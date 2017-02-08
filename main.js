53b17e6fdf9c3ce8fa788f6a319225dd7209285a
Kurt Wyckoff <kurtitservices@gmail.com>
2017-02-08 05:07:06 -0500

    got click event

---
+++ main.js
@@ -0,0 +1,88 @@
+
+
+//1) set up a process to click on section
+//2a) when is clicked
+//2) add a class to h1 section1, section 2, section 3
+//3)when h1 section 1 is clicked , that page becomes active,
+// color of h1 gets lighter and others
+//slide out of way
+//4)section 1 doesnotslide,section 2 and 3 slide up and down
+//5)when
+
+// var alphabetBox = document.querySelector(".alphabet");
+// target.addEventListener("click", processClick);
+
+function sectionClick (event) {
+
+  //  var target = event.target;
+
+
+  console.log("i'm working" );
+}
+
+  $(".section1").click(sectionClick);
+  $(".section2").click(sectionClick);
+  $(".section3").click(sectionClick);
+
+
+
+  // $(".section1").removeClass("open").addClass("close");
+  // $(".section2").removeClass("open").addClass("close");
+  // $(".section3").removeClass("open").addClass("close");
+  // target.removeClass("close").addClass( "open" );
+  //  $(".section1").addClass("open");
+  //  $(".section2").addClass("close");
+  //  $(".section3").addClass("close");
+  //target.classList.add("guessed");
+// }
+// if (event) {
+//   $(".section2 h1").click(sectionClick)
+//   $(".section1").addClass(close);
+//   $(".section2").addClass(open);
+//   $(".section3").addClass(close);
+// }
+// if (event) {
+//   $(".section3 h1").click(sectionClick)
+//   $(".section1").addClass(close);
+//   $(".section2").addClass(close);
+//   $(".section3").addClass(open);
+// }
+// }
+
+
+
+
+
+
+
+
+
+//   if(event) {
+//     $( "section1" ).addClass(function() {
+//       return "section1New";
+//     });
+//     if(event) {
+//       $( "section1" ).addClass(function() {
+//         return "section1New";
+//       });
+//
+// $( "ul li" ).addClass(function( index ) {
+//   return "item-" + index;
+// });
+//
+// $( "li:last" ).removeClass(function() {
+//   return $( this ).prev().attr( "class" );
+// });
+// function open () {
+//
+// }
+//
+// $(".section1").addclass(open)
+// $(".section2").addclass(close)
+// $(".section3").addclass(close)
+
+
+
+
+//   var alphabetBox = document.querySelector(".alphabet");
+// alphabetBox.addEventListener('click', section1Click);
